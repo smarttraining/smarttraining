6459ecaad2395b6db42f99940564998280d3387c
Хадиша Мусахан <khadi_97@mail.ru>
2018-04-08 21:42:22 +0600

    first demo version

---
+++ smarttraining/smarttraining/Pages/MasterPageItem.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace smarttraining
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPageItem : ContentPage
+    {
+        public MasterPageItem()
+        {
+            InitializeComponent();
+        }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type TargetType { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+    //public class MyModel : INotifyPropertyChanged
+    //{
+
+
+    //    private Color _backgroundColor;
+
+    //    public Color BackgroundColor
+    //    {
+    //        get { return _backgroundColor; }
+    //        set
+    //        {
+    //            _backgroundColor = value;
+
+    //            if (PropertyChanged != null)
+    //            {
+    //                PropertyChanged(this, new PropertyChangedEventArgs("BackgroundColor"));
+    //            }
+    //        }
+    //    }
+
+    //    public void SetColors(bool isSelected)
+    //    {
+    //        if (isSelected)
+    //        {
+    //            BackgroundColor = Color.FromRgb(0.20, 0.20, 1.0);
+    //        }
+    //        else
+    //        {
+    //            BackgroundColor = Color.FromRgb(0.95, 0.95, 0.95);
+    //        }
+    //    }
+    //}
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ smarttraining/smarttraining/Pages/Registration.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace smarttraining
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registration : ContentPage
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+       
+        async void Reg(object sender, EventArgs e)
+        {
+			if (Email.Text == null || Email.Text.Equals(""))
+			{
+				await DisplayAlert("Ошибка", "Email должен быть указан", "ОК");
+			}
+			else if (Password.Text == null || Password.Text.Equals(""))
+			{
+				await DisplayAlert("Ошибка", "Password должен быть указан", "ОК");
+			}
+
+			else
+			{
+				if (Password.Text.Length < 3)
+				{
+				await DisplayAlert("Ошибка", "Пароль слишком короткий", "ОК");
+				}
+				else if(!Email.Text.Contains("@"))
+				{
+					await DisplayAlert("Ошибка", "Указанный email неправильный", "ОК");
+				}
+				else
+				{
+					if (!Password.Text.Equals(PasswordCheck.Text))
+					{
+						await DisplayAlert("Ошибка", "Указанный email неправильный", "ОК");
+					}
+					else
+					{
+						User.Register(Email.Text, Password.Text);
+						User.Login(Email.Text, Password.Text);
+						await Navigation.PushModalAsync(new Registration2(User.profile.U_Id, User.profile.U_Key));
+					}
+				}
+			}
+        }
+    }
+}
